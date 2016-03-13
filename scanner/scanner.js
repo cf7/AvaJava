42c82f1fbd113ab719d4f46e8b6544e8a3a99802
cf7 <cfranco7@lion.lmu.edu>
2016-03-12 18:20:40 -0800

    Notes in scanner to understand what is happening

--- scanner/scanner.js
+++ scanner/scanner.js
@@ -13,19 +13,32 @@ var threeCharacterTokens = /...|\*\*\*/;
 var FLOAT = /^(\.\d+|\d+(\.\d+)?)([Ee][+-]?\d+)?$/;
 var stringTokens; // code for strings
 
+// When other files, such as test-scanner, require scanner.js,
+// module.exports is the function that they require
+// so in test-scanner, module.exports is being required into a 
+// variable named "scan", in the first describe-it sequence of tests,
+// notice that in the first "it" the "scan" variable is being called,
+// and filename and callback function are being passed in
 module.exports = function(filename, callback) {
+  // setting up the scanning action
   var baseStream = fs.createReadStream(filename, { encoding: 'utf8' });
   baseStream.on('error', function(err) {
     return error(err);
   });
 
+  // setting it to scan line by line
   var stream = byline(baseStream, { keepEmptyLines: true });
   var tokens = [];
   var linenumber = 0;
   stream.on('readable', function() {
+    // read in the next line and send it to the scan function
+    // defined later in scanner.js
     return scan(stream.read(), ++linenumber, tokens);
   });
 
+    // the stream looks for the word "end" to stop scanning,
+    // if it encounters it, the last token pushed into tokens
+    // is the EOF (End Of File) token
   return stream.once('end', function() {
     tokens.push({
       kind: 'EOF',
@@ -35,12 +48,17 @@ module.exports = function(filename, callback) {
   });
 };
 
+// when module.exports calls this scan function, it passes
+// in an entire line of symbols stored in an array (including
+// space, tab, and newline characters)
 var scan = function (line, lineNumber, tokens) {
   if (!line) {
     return;
   } else {
     var start = 0;
     var pos = 0;
+    // function that will store token data to be outputted by the scanner
+    // as a whole
     var emit = function (kind, lexeme) {
       tokens.push({ kind, lexeme: lexeme || kind, line: lineNumber, col: start+1 });
     }
@@ -48,19 +66,24 @@ var scan = function (line, lineNumber, tokens) {
 
     while (true) {
 
+      // skip spaces
       while (/\s/.test(line[pos])) {
         pos++;
       }
 
       start = pos;
 
+      // if reached end of line, leave while loop
       if (pos >= line.length) {
         break;
       }
+      // if reached comments in the line, leave while loop
       if (line[pos] === '/' && line[pos + 1] === '/') {
         break;
       }
 
+      // checking for 1, 2, and 3 symbol groups to tokenize
+      // checks them against regular expressions defined above
       if (threeCharacterTokens.test(line.substring(pos, pos + 3))) {
         emit(line.substring(pos, pos + 3));
         pos += 3;
