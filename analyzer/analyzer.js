0c395bed83ff96d1d6f0a4aee5c0dce1d3eb7d04
cf7 <cfranco7@lion.lmu.edu>
2016-03-15 16:19:58 -0700

    re-converted analyzer back to function

--- analyzer/analyzer.js
+++ analyzer/analyzer.js
@@ -1,21 +1,49 @@
 "use strict";
-var error = require('../error.js');
-// var variableDeclaration = require('../entities/VariableDeclaration');
+var AnalysisContext, VariableDeclaration, error;
 
-class AnalysisContext {
+error = require('./error');
 
-    constructor(parent) {
-        this.symbolTable = {};
+VariableDeclaration = require('./entities/VariableDeclaration');
+
+AnalysisContext = (function() {
+  function AnalysisContext(parent) {
+    this.parent = parent;
+    this.symbolTable = {};
+  }
+
+  AnalysisContext.initialContext = function() {
+    return new AnalysisContext(null);
+  };
+
+  AnalysisContext.prototype.createChildContext = function() {
+    return new AnalysisContext(this);
+  };
+
+  AnalysisContext.prototype.variableMustNotBeAlreadyDeclared = function(token) {
+    if (this.symbolTable[token.lexeme]) {
+      return error("Variable " + token.lexeme + " already declared", token);
     }
+  };
 
-    initialContext() {
-        new AnalysisContext(null);
+  AnalysisContext.prototype.addVariable = function(name, entity) {
+    return this.symbolTable[name] = entity;
+  };
+
+  AnalysisContext.prototype.lookupVariable = function(token) {
+    var variable;
+    variable = this.symbolTable[token.lexeme];
+    if (variable) {
+      return variable;
+    } else if (!this.parent) {
+      error("Variable " + token.lexeme + " not found", token);
+      return VariableDeclaration.ARBITRARY;
+    } else {
+      return this.parent.lookupVariable(token);
     }
+  };
 
-    createChildContext() {
-        new AnalysisContext(this);
-    }
+  return AnalysisContext;
 
-}
+})();
 
 exports.initialContext = AnalysisContext.initialContext;
