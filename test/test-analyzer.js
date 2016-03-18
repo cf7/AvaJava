443c3fac14833f23b40b6d743dcb5cdb53c950d8
cf7 <cfranco7@lion.lmu.edu>
2016-03-17 23:08:57 -0700

    changed analyzer code to use forEach loop instead

--- test/test-analyzer.js
+++ test/test-analyzer.js
@@ -4,27 +4,23 @@ var should = require('should');
 var scan = require('../scanner/scanner.js');
 var parse = require('../parser/parser.js');
 var error = require('../error.js');
-error.quiet = true;
+error.quiet = false;
 var TEST_DIR = 'test/data/semantic-errors';
 
 describe('The analyzer detects an error for', function() {
-  var check, i, len, name, ref, results;
-  ref = fs.readdirSync(TEST_DIR);
-  results = [];
-  for (i = 0, len = ref.length; i < len; i++) {
-    name = ref[i];
-    check = name.replace(/-/g, ' ').replace(/\.iki$/, '');
-    results.push(it(check, function(done) {
-      return scan(path.join(TEST_DIR, name), function(tokens) {
-        var priorErrorCount, program;
-        priorErrorCount = error.count;
-        program = parse(tokens);
-        error.count.should.equal(priorErrorCount);
-        program.analyze();
-        error.count.should.be.above(priorErrorCount);
-        return done();
+  return fs.readdirSync(TEST_DIR).forEach( function (name) {
+    if (name !== ".DS_Store") {
+      var check = name.replace(/-/g, ' ').replace(/\.ava$/, '');
+      return it(check, function(done) {
+        return scan(path.join(TEST_DIR, name), function(tokens) {
+          var priorErrorCount = error.count;
+          var program = parse(tokens);
+          error.count.should.equal(priorErrorCount);
+          program.analyze();
+          error.count.should.be.above(priorErrorCount);
+          return done();
+        });
       });
-    }));
-  }
-  return results;
+    }
+  });
 });
