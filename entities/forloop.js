21312500f5372152ea1403f9d09df132f9147d8e
cf7 <cfranco7@lion.lmu.edu>
2016-04-12 01:19:58 -0700

    forloop's analyze function

--- entities/forloop.js
+++ entities/forloop.js
@@ -16,7 +16,11 @@ class ForLoop {
     }
 
     analyze(context) {
+        this.exp.analyze(context); // analyze with broader context
+        var localContext = context.createChildContext();
+        localContext.addVariable(this.id.lexeme, this.id);
         // should have own local context for body
+        return this.body.analyze(localContext);
         // check for iterable type
     }
 
